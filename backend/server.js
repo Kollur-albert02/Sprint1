3004f6a2e70fef2b4dcc3b117e9aef3384943d40
Kollur-albert02 <kolluralbert@gmail.com>
2026-09-17 11:24:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ app.get("/", (req, res) => {
     res.send("My new server is running");
 });
 
-const PORT = 5000;
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
